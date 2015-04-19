7783fc3cec321890c063b675172bc13a1d02a890
Garry Kling <garry.kling@gmail.com>
2015-04-19 00:53:47 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// 
+// main.cpp
+// LogDeck - Simple C++ logging
+//
+// Created by Garry Kling on 4/19/2015
+//
+
+// simple driver programs to test/demo LogDeck features
+
+#include <stdio>
+#include <string>
+#include <sstream>
+
+#include "Log.h"
+  
+
+int main(int argc, char * argv[])
+{
+
+ return 0;
+
+}
+
+
+
